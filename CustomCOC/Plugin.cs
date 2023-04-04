3b7d6975695e89513bdd7edc0e78302f5478544e
Kaylie <lennon.m.craig@gmail.com>
2023-04-03 23:17:16 -0400

    Add everything

---
+++ CustomCOC/Plugin.cs
@@ -0,0 +1,44 @@
+﻿using BepInEx;
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+using Utilla;
+
+namespace CustomCOC
+{
+    /// <summary>
+    /// This is your mod's main class.
+    /// </summary>
+
+    /* This attribute tells Utilla to look for [ModdedGameJoin] and [ModdedGameLeave] */
+    [ModdedGamemode]
+    [BepInDependency("org.legoandmars.gorillatag.utilla", "1.5.0")]
+    [BepInPlugin(PluginInfo.GUID, PluginInfo.Name, PluginInfo.Version)]
+    public class Plugin : BaseUnityPlugin
+    {
+        public string logOutput = "";
+        void Start()
+        {
+            /* A lot of Gorilla Tag systems will not be set up when start is called /*
+			/* Put code in OnGameInitialized to avoid null references */
+
+            Utilla.Events.GameInitialized += OnGameInitialized;
+        }
+
+        void OnGameInitialized(object sender, EventArgs e)
+        {
+            GameObject cocHeaderObject = GameObject.Find("/Level/lower level/UI/CodeOfConduct");
+            Text cocHeaderText = cocHeaderObject.GetComponent<Text>();
+            cocHeaderText.text = "FUN STUFF! YIPPEE!";
+
+            GameObject cocObject = GameObject.Find("/Level/lower level/UI/Tree Room Texts/COC Text");
+            Text cocText = cocObject.GetComponent<Text>();
+            cocText.text = "PRETEND I HAVE STUFF HERE LOL";
+        }
+
+        void Update()
+        {
+
+        }
+    }
+}
---
+++ CustomCOC/PluginInfo.cs
@@ -0,0 +1,12 @@
+﻿namespace CustomCOC
+{
+    /// <summary>
+    /// This class is used to provide information about your mod to BepInEx.
+    /// </summary>
+    internal class PluginInfo
+    {
+        public const string GUID = "com.kaylie.gorillatag.customcoc";
+        public const string Name = "CustomCOC";
+        public const string Version = "1.0.0";
+    }
+}
